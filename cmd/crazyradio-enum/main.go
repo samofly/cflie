25ce668e8a2b29b9c16fb5cf11fe63d78ed7790d
Ivan Krasin <imkrasin@gmail.com>
2013-08-12 22:34:37 -0700

    Implement Read and Write for crazyradio.Device, rebaze crazyradio-enum on top of crazyradio library. I am unable to verify if that works at all, since my home desktop is about die

--- cmd/crazyradio-enum/main.go
+++ cmd/crazyradio-enum/main.go
@@ -7,83 +7,16 @@ import (
 	"os"
 	"time"
 
-	"github.com/kylelemons/gousb/usb"
+	"github.com/krasin/crazyradio"
 )
 
-type Request uint8
-
-const (
-	SET_RADIO_CHANNEL Request = 0x01
-	SET_RADIO_ADDRESS Request = 0x02
-	SET_DATA_RATE     Request = 0x03
-	SET_RADIO_POWER   Request = 0x04
-	SET_RADIO_ARD     Request = 0x05
-	SET_RADIO_ARC     Request = 0x06
-	ACK_ENABLE        Request = 0x10
-	SET_CONT_CARRIER  Request = 0x20
-	CHANNEL_SCANN     Request = 0x21
-	LAUNCH_BOOTLOADER Request = 0xFF
-)
-
-type DataRate uint16
-
-const (
-	DATA_RATE_250K DataRate = 0
-	DATA_RATE_1M   DataRate = 1
-	DATA_RATE_2M   DataRate = 2
-
-	RADIO_POWER_M18dBm = 0
-	RADIO_POWER_M12dBm = 1
-	RADIO_POWER_M6dBm  = 2
-	RADIO_POWER_0dBm   = 3
-)
-
-func control(d *usb.Device, req Request, val uint16, data []byte) error {
-	_, err := d.Control(usb.REQUEST_TYPE_VENDOR, uint8(req), val, 0, data)
-	return err
-}
-
-func initDongle(d *usb.Device, ch uint16, rate DataRate) (err error) {
-	if err = control(d, SET_DATA_RATE, uint16(DATA_RATE_250K), nil); err != nil {
-		return
-	}
-	if err = control(d, SET_RADIO_CHANNEL, 2, nil); err != nil {
-		return
-	}
-	if err = control(d, SET_CONT_CARRIER, 0, nil); err != nil {
-		return
-	}
-	if err = control(d, SET_RADIO_ADDRESS, 0, []byte{0xE7, 0xE7, 0xE7, 0xE7, 0xE7}); err != nil {
-		return
-	}
-	if err = control(d, SET_RADIO_POWER, RADIO_POWER_0dBm, nil); err != nil {
-		return
-	}
-	if err = control(d, SET_RADIO_ARC, 3, nil); err != nil {
-		return
-	}
-	if err = control(d, SET_RADIO_ARD, 0x80|32, nil); err != nil {
-		return
-	}
-	if err = control(d, SET_RADIO_ARC, 10, nil); err != nil {
-		return
-	}
-	if err = control(d, SET_RADIO_CHANNEL, ch, nil); err != nil {
-		return
-	}
-	if err = control(d, SET_DATA_RATE, uint16(rate), nil); err != nil {
-		return
-	}
-	return
-}
-
-func reader(in usb.Endpoint, ch chan<- []byte) {
+func reader(d crazyradio.Device, ch chan<- []byte) {
 	buf := make([]byte, 64)
 	var cnt uint64
 	var lastCnt uint64
 	startTime := time.Now()
 	for {
-		n, err := in.Read(buf)
+		n, err := d.Read(buf)
 		if err != nil {
 			log.Printf("Error: reader: %v", err)
 			continue
@@ -130,10 +63,10 @@ func consume(cnt int, readCh <-chan []byte) {
 	}
 }
 
-func sendPackage(out usb.Endpoint, readCh <-chan []byte, p []byte) (err error) {
+func sendPackage(d crazyradio.Device, readCh <-chan []byte, p []byte) (err error) {
 	// log.Printf("sendPackage: %v", p)
 	consume(0, readCh)
-	_, err = out.Write(p)
+	_, err = d.Write(p)
 	if err != nil {
 		return fmt.Errorf("sendPackage: %v", err)
 	}
@@ -141,7 +74,7 @@ func sendPackage(out usb.Endpoint, readCh <-chan []byte, p []byte) (err error) {
 	return
 }
 
-func writer(out usb.Endpoint, writeCh <-chan []byte, readCh <-chan []byte) {
+func writer(d crazyradio.Device, writeCh <-chan []byte, readCh <-chan []byte) {
 	buf := []byte{0xFF}
 	cnt := 0
 	for {
@@ -151,7 +84,7 @@ func writer(out usb.Endpoint, writeCh <-chan []byte, readCh <-chan []byte) {
 		default:
 			p = buf
 		}
-		err := sendPackage(out, readCh, p)
+		err := sendPackage(d, readCh, p)
 		if err != nil {
 			log.Printf("Error: writer: %v", err)
 		}
@@ -162,78 +95,35 @@ func writer(out usb.Endpoint, writeCh <-chan []byte, readCh <-chan []byte) {
 	}
 }
 
-func listDongles() error {
-	ctx := usb.NewContext()
-	defer ctx.Close()
+func fail(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, args...)
+	os.Exit(1)
+}
 
-	devs, err := ctx.ListDevices(func(desc *usb.Descriptor) bool {
-		if desc.Vendor == 0x1915 && desc.Product == 0x7777 {
-			return true
-		}
-		return false
-	})
-
-	defer func() {
-		for _, d := range devs {
-			d.Close()
-		}
-	}()
-
+func main() {
+	list, err := crazyradio.ListDevices()
 	if err != nil {
-		return err
+		fail("Unable to list USB devices: %v\n", err)
 	}
-
-	if len(devs) == 0 {
-		return fmt.Errorf("No CrazyRadio dongles found!")
+	if len(list) == 0 {
+		fail("No CrazyRadio dongle present")
 	}
-
-	for _, dev := range devs {
-		fmt.Printf("CrazyRadio USB dongle v%s\n", dev.Device)
-	}
-
-	controller := devs[0]
-	controller.ReadTimeout = 50 * time.Millisecond
-
-	in, err := controller.OpenEndpoint(
-		/* config */ 1,
-		/* iface */ 0,
-		/* setup */ 0,
-		/* endpoint */ 0x81|uint8(usb.ENDPOINT_DIR_IN))
+	d, err := crazyradio.Open(list[0])
 	if err != nil {
-		return fmt.Errorf("OpenEndpoint(IN): %v", err)
+		fail("Unable to open CrazyRadio (try running as root?): %v\n", err)
 	}
-
-	out, err := controller.OpenEndpoint(
-		/* config */ 1,
-		/* iface */ 0,
-		/* setup */ 0,
-		/* endpoint */ 1|uint8(usb.ENDPOINT_DIR_OUT))
-	if err != nil {
-		return fmt.Errorf("OpenEndpoint(OUT): %v", err)
-	}
-
-	if err = initDongle(controller, 10, DATA_RATE_1M); err != nil {
-		return fmt.Errorf("initDongle: %v", err)
-	}
+	defer d.Close()
 
 	readCh := make(chan []byte, 10)
 	writeCh := make(chan []byte)
 
-	go reader(in, readCh)
-	go writer(out, writeCh, readCh)
+	go reader(d, readCh)
+	go writer(d, writeCh, readCh)
 
 	writeCh <- []byte{60, 0, 0, 0, 0, 0, 0, 0, 128, 250, 117, 61, 64, 48, 117}
 
 	fmt.Printf("Press Ctrl+C to exit\n")
 	for {
 		time.Sleep(time.Second)
-	}
-	return nil
-}
-
-func main() {
-	if err := listDongles(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
 	}
 }
--- device.go
+++ device.go
@@ -2,8 +2,37 @@ package crazyradio
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/kylelemons/gousb/usb"
+)
+
+type Request uint8
+type DataRate uint16
+
+const (
+	SET_RADIO_CHANNEL Request = 0x01
+	SET_RADIO_ADDRESS Request = 0x02
+	SET_DATA_RATE     Request = 0x03
+	SET_RADIO_POWER   Request = 0x04
+	SET_RADIO_ARD     Request = 0x05
+	SET_RADIO_ARC     Request = 0x06
+	ACK_ENABLE        Request = 0x10
+	SET_CONT_CARRIER  Request = 0x20
+	CHANNEL_SCANN     Request = 0x21
+	LAUNCH_BOOTLOADER Request = 0xFF
+
+	DATA_RATE_250K DataRate = 0
+	DATA_RATE_1M   DataRate = 1
+	DATA_RATE_2M   DataRate = 2
+
+	RADIO_POWER_M18dBm = 0
+	RADIO_POWER_M12dBm = 1
+	RADIO_POWER_M6dBm  = 2
+	RADIO_POWER_0dBm   = 3
+
+	DefaultChannel  = 10
+	DefaultDataRate = DATA_RATE_250K
 )
 
 var ErrDeviceNotFound = fmt.Errorf("Device not found")
@@ -11,6 +40,8 @@ var ErrTooManyDevicesMatch = fmt.Errorf("Too many devices match (> 1)")
 
 type Device interface {
 	Close() error
+	Read(p []byte) (n int, err error)
+	Write(p []byte) (n int, err error)
 }
 
 // Open opens a CrazyRadio USB dongle
@@ -35,13 +66,88 @@ func Open(info DeviceInfo) (Device, error) {
 	if len(d) > 1 {
 		return nil, ErrTooManyDevicesMatch
 	}
-	return &device{d: d[0]}, nil
+
+	res := &device{d: d[0]}
+	if err = res.initDongle(DefaultChannel, DefaultDataRate); err != nil {
+		res.Close()
+		return nil, fmt.Errorf("Unable to init dongle: %v", err)
+	}
+	return res, nil
 }
 
 type device struct {
-	d *usb.Device
+	d   *usb.Device
+	in  usb.Endpoint
+	out usb.Endpoint
+}
+
+func (d *device) Read(p []byte) (n int, err error) {
+	return d.in.Read(p)
+}
+
+func (d *device) Write(p []byte) (n int, err error) {
+	return d.out.Write(p)
 }
 
 func (d *device) Close() error {
 	return d.d.Close()
+}
+
+func (d *device) control(req Request, val uint16, data []byte) error {
+	_, err := d.d.Control(usb.REQUEST_TYPE_VENDOR, uint8(req), val, 0, data)
+	return err
+}
+
+func (d *device) initDongle(ch uint16, rate DataRate) (err error) {
+	d.d.ReadTimeout = 50 * time.Millisecond
+
+	d.in, err = d.d.OpenEndpoint(
+		/* config */ 1,
+		/* iface */ 0,
+		/* setup */ 0,
+		/* endpoint */ 0x81|uint8(usb.ENDPOINT_DIR_IN))
+	if err != nil {
+		return fmt.Errorf("OpenEndpoint(IN): %v", err)
+	}
+
+	d.out, err = d.d.OpenEndpoint(
+		/* config */ 1,
+		/* iface */ 0,
+		/* setup */ 0,
+		/* endpoint */ 1|uint8(usb.ENDPOINT_DIR_OUT))
+	if err != nil {
+		return fmt.Errorf("OpenEndpoint(OUT): %v", err)
+	}
+
+	if err = d.control(SET_DATA_RATE, uint16(DATA_RATE_250K), nil); err != nil {
+		return
+	}
+	if err = d.control(SET_RADIO_CHANNEL, 2, nil); err != nil {
+		return
+	}
+	if err = d.control(SET_CONT_CARRIER, 0, nil); err != nil {
+		return
+	}
+	if err = d.control(SET_RADIO_ADDRESS, 0, []byte{0xE7, 0xE7, 0xE7, 0xE7, 0xE7}); err != nil {
+		return
+	}
+	if err = d.control(SET_RADIO_POWER, RADIO_POWER_0dBm, nil); err != nil {
+		return
+	}
+	if err = d.control(SET_RADIO_ARC, 3, nil); err != nil {
+		return
+	}
+	if err = d.control(SET_RADIO_ARD, 0x80|32, nil); err != nil {
+		return
+	}
+	if err = d.control(SET_RADIO_ARC, 10, nil); err != nil {
+		return
+	}
+	if err = d.control(SET_RADIO_CHANNEL, ch, nil); err != nil {
+		return
+	}
+	if err = d.control(SET_DATA_RATE, uint16(rate), nil); err != nil {
+		return
+	}
+	return
 }
